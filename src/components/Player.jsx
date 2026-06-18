8127190021f118c5450e1e2ef920c1e00972f17a
Joseph Madamba <madamba.joehu@outlook.com>
2026-06-17 20:29:42 -0700

    Rework player layout to use fixed width to fix progress bar resizing based on audio length
    Also fixes progress bar resizing constantly every second on other browsers that have non-fixed-width text.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -202,7 +202,7 @@ const Player = (props) => {
           className="w-full flex items-center justify-center"
           style={{ marginTop: "10px" }}
         >
-          {toReadableTime(trackProgress)}
+          <div className="text-right flex-1">{toReadableTime(trackProgress)}</div>
           <input
             type="range"
             step="1"
@@ -214,12 +214,9 @@ const Player = (props) => {
             onClick={onScrubEnd}
             onKeyUp={onScrubEnd}
           />
-          {toReadableTime(duration)}
+          <div className="text-left flex-1">{toReadableTime(duration)}</div>
         </div>
-        <div
-          className="flex items-center justify-evenly"
-          style={{ marginTop: "10px" }}
-        >
+        <div className="audioControls flex items-center justify-between">
           <img
             className="audioIcon"
             onClick={() => onReplay()}
